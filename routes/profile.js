f2db76a0360ff15cbee97a0d68284a7fdb6a710e
JaimeGSandoval <jaime.devs@gmail.com>
2023-08-05 15:49:49 -0700

    Add photgrapher names to posts in feed

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,8 @@ const upload = require('../middleware/multer');
 const profileController = require('../controllers/profile.js');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-router.get('/user', ensureAuth, profileController.getUserProfile);
+router.get('/:userName', profileController.getProfileByUsername);
+router.get('/user', ensureAuth, profileController.getProfileById);
 router.get('/form', ensureAuth, profileController.getProfileForm);
 router.post('/create', ensureAuth, upload.single('uploaded_file'), profileController.createProfile);
 
